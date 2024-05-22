2063f288f7fd2df55c127e2cb5a329840257fa79
Vee227 <157512338+Vee227@users.noreply.github.com>
2024-05-22 17:41:47 +0200

    TC3_Refactor

--- helpalf/src/string_calculator.js
+++ helpalf/src/string_calculator.js
@@ -2,13 +2,7 @@
 function HelpAlf() {}
 HelpAlf.prototype.add = function(map) {
 	const rows = map.trim().split('/n');
-	if(map == '........../n ........../n ........../n ........../n ........../n ........../n'){
-		return 'Spaceship lost forever';
-	}
-	if(map == '........../n ........../n ....X...../n ........../n ........../n ........../n'){
-		return '[4,3]';
-	}
-
+	
 	for (let y = 0; y < rows.length; y++) {
         const x = rows[y].indexOf('X');
         if (x !== -1) {
@@ -16,6 +10,7 @@ HelpAlf.prototype.add = function(map) {
             return '['+ (x-1) + ','+ (rows.length - 2 - y)+ ']';
         }
     }
+	return 'Spaceship lost forever';
     
 	
 };
