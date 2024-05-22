32e83d55faaba9ad7651b7915810f4fbec1f5d15
Vee227 <157512338+Vee227@users.noreply.github.com>
2024-05-22 17:39:52 +0200

    TC3_Green

--- helpalf/src/string_calculator.js
+++ helpalf/src/string_calculator.js
@@ -1,10 +1,21 @@
 // StringCalculator.js
 function HelpAlf() {}
 HelpAlf.prototype.add = function(map) {
+	const rows = map.trim().split('/n');
 	if(map == '........../n ........../n ........../n ........../n ........../n ........../n'){
 		return 'Spaceship lost forever';
 	}
 	if(map == '........../n ........../n ....X...../n ........../n ........../n ........../n'){
 		return '[4,3]';
 	}
+
+	for (let y = 0; y < rows.length; y++) {
+        const x = rows[y].indexOf('X');
+        if (x !== -1) {
+            // Die y-Koordinate umkehren
+            return '['+ (x-1) + ','+ (rows.length - 2 - y)+ ']';
+        }
+    }
+    
+	
 };
