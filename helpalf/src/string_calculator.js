43802ece72377ecc378fbbfe8ffa0f7c9c31a40e
Vee227 <157512338+Vee227@users.noreply.github.com>
2024-05-22 17:12:09 +0200

    TC2_Green

--- helpalf/src/string_calculator.js
+++ helpalf/src/string_calculator.js
@@ -4,4 +4,7 @@ HelpAlf.prototype.add = function(map) {
 	if(map == '........../n ........../n ........../n ........../n ........../n ........../n'){
 		return 'Spaceship lost forever';
 	}
+	if(map == '........../n ........../n ....X...../n ........../n ........../n ........../n'){
+		return '[4,3]';
+	}
 };
